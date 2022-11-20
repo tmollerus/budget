a8f7671a6a024b896984c04c71f77eb08f409e5d
Tom Mollerus <tom@mollerus.net>
2022-11-19 22:21:51 -0500

    made footer deploy date based on env var

--- packages/budget-ui/src/components/Footer.tsx
+++ packages/budget-ui/src/components/Footer.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import { dateFormat } from '../utils/format';
 import { useStyles } from './Footer.styles';
 
 export const Footer = (props: any) => {
   const classes = useStyles();
   const currentYear = new Date().getFullYear();
+  const deployDate = dateFormat(process.env.DEPLOY_DATE || new Date(), 'yyyy-mm-dd');
+  const deployTime = dateFormat(process.env.DEPLOY_DATE || new Date(), 'h:MMtt');
 
   return (
     <div className={classes.footer}>
-      Version {process.env.REACT_APP_VERSION} deployed on 2021-04-23 at 8:04am. Copyright &copy;2014-{currentYear} Tom
-      Mollerus
+      Version {process.env.REACT_APP_VERSION} deployed on {deployDate} at {deployTime}. Copyright
+      &copy;2014-
+      {currentYear} Tom Mollerus
     </div>
   );
 };
