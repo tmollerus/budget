a25de37264b5e29c739fa5db7d72b412bf398ead
Tom Mollerus <tom@mollerus.net>
2022-12-12 01:48:31 -0500

    feat: added okta user lookup

--- packages/budget-api/src/v1/handlers/auth/login/item.ts
+++ packages/budget-api/src/v1/handlers/auth/login/item.ts
@@ -1,14 +1,17 @@
 import { Context, APIGatewayProxyResult, APIGatewayEvent } from 'aws-lambda';
+import { getOktaUser } from '../../../managers/okta';
+import { getAuthToken } from '../../../utils/event';
 
 export const getHandler = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
   console.log(`Event: ${JSON.stringify(event, null, 2)}`);
   console.log(`Context: ${JSON.stringify(context, null, 2)}`);
 
+  const authToken = getAuthToken(event);
+  const user = getOktaUser(authToken);
+
   return {
     statusCode: 200,
-    body: JSON.stringify({
-        message: 'hello world',
-    }),
+    body: JSON.stringify({user}),
   };
 
   // // Get the auth token
