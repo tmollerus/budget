101804df4644ae9552874156422fb24def422294
Tom Mollerus <tom@mollerus.net>
2022-12-16 17:16:56 -0500

    feat: corrected table name, added uuid

--- packages/budget-api/src/v1/managers/postgres.ts
+++ packages/budget-api/src/v1/managers/postgres.ts
@@ -1,4 +1,5 @@
 import { Client } from 'pg';
+import { v4 as uuidv4 } from 'uuid';
 import { migration001 } from '../schema/postgres/migrations/001-createBudgetsTable.sql';
 import { migration002 } from '../schema/postgres/migrations/002-createTypesTable.sql';
 import { migration003 } from '../schema/postgres/migrations/003-createUsersTable.sql';
@@ -168,10 +169,13 @@ export const getClient = async (): Promise<any> => {
   const client = await getClient();
 
   try {
-    const result = await client.query(`
-      INSERT INTO budgets (budget_guid, guid, "settledDate", type_id, amount, paid, label)
-      VALUES ($1, $2, $3, $4, $5, $6, $7)
-    `, [budgetGuid, budgetItem.guid, budgetItem.settledDate, budgetItem.type_id, budgetItem.amount, budgetItem.paid, budgetItem.label]);
+    const sql = `
+      INSERT INTO items (budget_guid, guid, "settledDate", type_id, amount, paid, label, dateCreated, dateModified)
+      VALUES ($1, $2, $3, $4, $5, $6, $7, NOW(), NOW())
+    `;
+    const params = [budgetGuid, uuidv4(), budgetItem.settledDate, budgetItem.type_id, budgetItem.amount, budgetItem.paid, budgetItem.label];
+    console.log('Executing sql', sql, params);
+    const result = await client.query(sql, params);
     console.log(result);
     return result;
   } catch (err) {
