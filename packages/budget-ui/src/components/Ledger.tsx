65fe9d33829a11f341800a71d28862b60238b2ea
Tom Mollerus <tom@mollerus.net>
2023-01-26 21:24:27 -0500

    fix: removed console logging statement

--- packages/budget-ui/src/components/Ledger.tsx
+++ packages/budget-ui/src/components/Ledger.tsx
@@ -69,7 +69,6 @@ export const Ledger = () => {
     event?: React.MouseEvent<HTMLElement, MouseEvent>,
   ): boolean => {
     event?.preventDefault();
-    console.log('Scrolling to', target);
     const el = document.getElementById(target);
 
     if (el) {
