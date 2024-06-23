a2081981e080e8286db96952bc9b778a91120d63
Tom Mollerus <tom@mollerus.net>
2024-06-23 07:11:50 -0400

    feat: added categories to api

--- packages/budget-api/src/v1/schema/postgres/migrations/005-createCategoriesTable.sql.test.ts
+++ packages/budget-api/src/v1/schema/postgres/migrations/005-createCategoriesTable.sql.test.ts
@@ -1,9 +1,9 @@
-import { migration004 } from './004-createItemsTable.sql';
+import { migration005 } from './005-createCategoriesTable.sql';
 
 describe('Categories table migration', () => {
   test('Contains correct, needed SQL', () => {
-    expect(migration004).toContain('CREATE TABLE IF NOT EXISTS categories');
-    expect(migration004).toContain('PRIMARY KEY');
-    expect(migration004).toContain('CREATE UNIQUE INDEX IF NOT EXISTS');
+    expect(migration005).toContain('CREATE TABLE IF NOT EXISTS categories');
+    expect(migration005).toContain('PRIMARY KEY');
+    expect(migration005).toContain('CREATE UNIQUE INDEX IF NOT EXISTS');
   });
 });
--- packages/budget-api/src/v1/schema/postgres/migrations/006-createSubcategoriesTable.sql.test.ts
+++ packages/budget-api/src/v1/schema/postgres/migrations/006-createSubcategoriesTable.sql.test.ts
@@ -1,9 +1,9 @@
-import { migration004 } from './004-createItemsTable.sql';
+import { migration006 } from './006-createSubcategoriesTable.sql';
 
 describe('Categories table migration', () => {
   test('Contains correct, needed SQL', () => {
-    expect(migration004).toContain('CREATE TABLE IF NOT EXISTS subcategories');
-    expect(migration004).toContain('PRIMARY KEY');
-    expect(migration004).toContain('CREATE UNIQUE INDEX IF NOT EXISTS');
+    expect(migration006).toContain('CREATE TABLE IF NOT EXISTS subcategories');
+    expect(migration006).toContain('PRIMARY KEY');
+    expect(migration006).toContain('CREATE UNIQUE INDEX IF NOT EXISTS');
   });
 });
