a5ba730cd956ce49627b12773b547c0d5f2fabab
Tom Mollerus <tom@mollerus.net>
2026-07-04 13:11:14 -0400

    chore: added v2 auth login endpoint

---
+++ packages/budget-api/src/v2/handlers/auth/login/item.ts
@@ -0,0 +1,23 @@
+import { Context, APIGatewayProxyResult, APIGatewayEvent } from 'aws-lambda';
+
+export const getHandler = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
+  console.log(`Event: ${JSON.stringify(event, null, 2)}`);
+  console.log(`Context: ${JSON.stringify(context, null, 2)}`);
+
+  try {
+    const budgetGuid = event.requestContext.authorizer?.lambda.budgetGuid;
+    const user = JSON.parse(event.requestContext.authorizer?.lambda.user);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({user: user, budgetGuid}),
+    };
+  } catch (err) {
+    console.log(err);
+  }
+  
+  return {
+    statusCode: 403,
+    body: '',
+  };
+};
