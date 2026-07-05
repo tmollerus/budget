1fe7ee56abf100623f5094276639d275fe5199b2
Tom Mollerus <tom@mollerus.net>
2026-07-05 19:12:55 -0400

    chore: added category and subcategory management

---
+++ packages/budget-api/src/v2/handlers/budgets/items.ts
@@ -0,0 +1,85 @@
+import { APIGatewayProxyResult, APIGatewayEvent } from 'aws-lambda';
+import { copyFromYear, deleteFromYear, getBudgetItemsByYear } from '../../../managers/dynamodb';
+
+export const getHandler = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult> => {
+  console.log(`Event: ${JSON.stringify(event, null, 2)}`);
+
+  const budgetGuid = event.pathParameters?.budgetGuid || '';
+  const year = event.queryStringParameters?.year || '';
+
+  try {
+    const items = await getBudgetItemsByYear(budgetGuid, year);
+    
+    return {
+      statusCode: 200,
+      body: JSON.stringify({items}),
+    };
+  } catch (err) {
+    console.log(err);
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({err}),
+    };
+  }
+};
+export const postHandler = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult> => {
+  console.log(`Event: ${JSON.stringify(event, null, 2)}`);
+
+  const budgetGuid = event.pathParameters?.budgetGuid || '';
+  const fromYear = event.queryStringParameters?.from || '';
+  const toYear = event.queryStringParameters?.to || '';
+
+  try {
+    const isCopySuccessful = await copyFromYear(budgetGuid, fromYear, toYear);
+    
+    if (isCopySuccessful) {
+      return {
+        statusCode: 200,
+        body: '{}',
+      };
+    } else {
+      return {
+        statusCode: 500,
+        body: '{"error": "Copy failed"}',
+      };
+    }
+  } catch (err) {
+    console.log(err);
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({err}),
+    };
+  }
+};
+
+export const deleteHandler = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult> => {
+  console.log(`Event: ${JSON.stringify(event, null, 2)}`);
+
+  const budgetGuid = event.pathParameters?.budgetGuid || '';
+  const fromYear = event.queryStringParameters?.from || '';
+
+  try {
+    const isDeleteSuccessful = await deleteFromYear(budgetGuid, fromYear);
+    
+    if (isDeleteSuccessful) {
+      return {
+        statusCode: 200,
+        body: '{}',
+      };
+    } else {
+      return {
+        statusCode: 500,
+        body: '{"error": "Delete failed"}',
+      };
+    }
+  } catch (err) {
+    console.log(err);
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({err}),
+    };
+  }
+};
